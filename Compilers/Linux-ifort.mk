76046c129a8003b9f86ff3aba89614ef2a822c55
vrx- <vrx@tamu.edu>
2019-08-01 12:58:43 -0500

    changed compiler to mpiifort

--- Compilers/Linux-ifort.mk
+++ Compilers/Linux-ifort.mk
@@ -73,7 +73,7 @@ endif
 ifdef USE_MPI
          CPPFLAGS += -DMPI
  ifdef USE_MPIF90
-               FC := mpif90
+               FC := mpiifort #mpif90
  else
              LIBS += -lfmpi-pgi -lmpi-pgi
  endif
